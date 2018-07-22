3e003071acd9318893eca800faca65a84fdf2643
Tswalano <moganegb@gmail.com>
2018-07-23 01:23:10 +0200

    JSON Data Complete

--- app/src/main/java/com/example/tswalano/edutrack/MainActivity.java
+++ app/src/main/java/com/example/tswalano/edutrack/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
     //    List
     ArrayList<DataModel> dataModels;
     HashMap<String, String> studentInfo;
-    ListView listView;
-    TextView txtName, txtSchool, txtGrade;
     private static StudentAdapter adapter;
     private ProgressDialog pDialog;
+
+    ListView listView;
+    TextView txtName, txtSchool, txtGrade;
+    ImageView btnNext, btnPrev;
+
+    int count = 0;
 
     //    JSON Var
     private String TAG = MainActivity.class.getSimpleName();
@@ -41,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         txtGrade = findViewById(R.id.txtStudentGrade);
         txtSchool = findViewById(R.id.txtStudentSchool);
         listView = findViewById(R.id.listView);
+        btnNext = findViewById(R.id.btnNext);
+        btnPrev = findViewById(R.id.btnPrev);
 
         dataModels = new ArrayList<>();
         studentInfo = new HashMap<>();
@@ -57,6 +65,10 @@ public class MainActivity extends AppCompatActivity {
             String jsonStr = handler.makeServiceCall(url);
             Log.e(TAG, "Response from URL: " + jsonStr);
 
+//            System.out.println(dataModels);
+            dataModels.clear();
+//            System.out.println(dataModels);
+
             if (jsonStr != null){
                 try {
                     JSONObject jsonObject = new JSONObject(jsonStr);
@@ -66,13 +78,14 @@ public class MainActivity extends AppCompatActivity {
 
 //                    Getting the json Array node
                     JSONArray school = jsonObject.getJSONArray("school");
-                    JSONObject schoolObject = school.getJSONObject(2);
+                    JSONObject schoolObject = school.getJSONObject(count);
 
                     String schoolName = schoolObject.getString("schoolName");
                     int year = schoolObject.getInt("year");
                     String grade = schoolObject.getString("grade");
 
                     JSONArray subjects = schoolObject.getJSONArray("subjects");
+
                     for (int i = 0; i < subjects.length(); i++){
                         JSONObject subjectObject = subjects.getJSONObject(i);
 
@@ -80,22 +93,22 @@ public class MainActivity extends AppCompatActivity {
                         String term = subjectObject.getString("term");
                         Integer mark = subjectObject.getInt("mark");
 
-                        System.out.println("DATA " + i +" : " + subject);
-
                         dataModels.add(new DataModel(subject, term, mark));
-                        studentInfo.put("name", name);
-                        studentInfo.put("surname", surname);
-                        studentInfo.put("schoolName", schoolName);
-                        studentInfo.put("grade", grade);
-                        studentInfo.put("year", Integer.toString(year));
                     }
+
+                    studentInfo.put("name", name);
+                    studentInfo.put("surname", surname);
+                    studentInfo.put("schoolName", schoolName);
+                    studentInfo.put("grade", grade);
+                    studentInfo.put("year", Integer.toString(year));
 
                 }catch (final JSONException e){
                     Log.e(TAG, "JSON parse error: " + e.getMessage());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            System.out.println();
+                            count = count - 1;
+                            System.err.println("JSON parse error: " + e.getMessage());
                             Toast.makeText(getApplicationContext(),
                                     "Something went wrong with the server",
                                     Toast.LENGTH_SHORT).show();
@@ -129,6 +142,33 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void results) {
             super.onPostExecute(results);
+
+            final GetStudents getStudents = new GetStudents();
+
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Check if json object is not last
+                    // Go to the next object
+                    count = count + 1;
+                    getStudents.execute();
+                }
+            });
+
+            btnPrev.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Check if json object is not first
+                    // Go to the previous object
+                    if(count != 0){
+                        count = count - 1;
+                    } else if(count == 0) {
+                        count = 0;
+                    } else {
+                    }
+                    getStudents.execute();
+                }
+            });
 
             txtName.setText(studentInfo.get("name") + " " + studentInfo.get("surname"));
             txtSchool.setText(studentInfo.get("schoolName"));
